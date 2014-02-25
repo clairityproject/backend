f4d5ad0bd4c7504233d4b08f4cbd424d37251e11
David Ogutu <davidogutu@gmail.com>
2014-02-25 14:50:11 -0500

    Admin interface updates

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from data.models import IP, Node, Location, DataPoint
 
 # Register your models here.
+
+admin.site.register(IP)
+admin.site.register(Node)
+admin.site.register(Location)
