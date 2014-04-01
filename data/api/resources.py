7043bd4083975049a73108579e707841a9788b23
Ubuntu <ubuntu@ip-172-31-20-171.us-west-2.compute.internal>
2014-04-01 19:39:33 +0000

    server changes

--- data/api/resources.py
+++ data/api/resources.py
@@ -1,18 +1,40 @@
 from tastypie.resources import ModelResource
-from data.models import IP, Node, DataPoint
+from tastypie import fields
+from data.models import IP, Node, DataPoint, Location
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 
 
 class AqResource(ModelResource):
     class Meta:
         allowed_methods = ['post','get']
 
-class NodeResource(AqResource):
-    class Meta:
-        queryset = Node.objects.all()
-
 class IPResource(AqResource):
     class Meta:
         queryset = IP.objects.all()
+	filtering={}
+	for field in IP.__dict__['_meta'].fields:
+	    filtering.update({field.name : ALL_WITH_RELATIONS})
+
 class DataPointResource(AqResource):
     class Meta:
         queryset = DataPoint.objects.all()
+	filtering={}
+	for field in DataPoint.__dict__['_meta'].fields:
+	    filtering.update({field.name : ALL_WITH_RELATIONS})
+
+class LocationResource(AqResource):
+    class Meta:
+        queryset = Location.objects.all()
+	filtering={}
+	for field in Location.__dict__['_meta'].fields:
+	    filtering.update({field.name : ALL_WITH_RELATIONS})
+
+class NodeResource(AqResource):
+    location = fields.ForeignKey(LocationResource, 'location',full=True)
+    ip = fields.ForeignKey(IPResource, 'ip',full=True)
+    class Meta:
+        queryset = Node.objects.all()
+	filtering={}
+	for field in Node.__dict__['_meta'].fields:
+	    filtering.update({field.name : ALL_WITH_RELATIONS})
+
