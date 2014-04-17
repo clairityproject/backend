96d8657e26489ca4e713e1a2394ec4f63f523121
David Ogutu <davidogutu@gmail.com>
2014-04-17 15:01:39 -0400

    chagned urls mappings

--- airquality/urls.py
+++ airquality/urls.py
@@ -15,9 +15,9 @@ urlpatterns = patterns('',
     #    API (coding team)
     #:::::::::::::::::::::::::::::
     url(r'^node/postdata/$','data.views.secret_post'),
-    url(r'^node/postdata/dylos/','data.views.secret_post_dylos'),
-    url(r'^node/postdata/met/','data.views.secret_post_met'),
-    url(r'^node/postdata/alphasense/$','data.views.secret_post_alphasense'),
+    url(r'^node/postdata/dylos','data.views.secret_post_dylos'),
+    url(r'^node/postdata/met','data.views.secret_post_met'),
+    url(r'^node/postdata/alphasense','data.views.secret_post_alphasense'),
     url(r'^api/', include(dataurls)),
 
     #:::::::::::::::::::::::::::::
