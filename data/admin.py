6a43820727b133becdb8b6395b4ff6f5af774893
David Ogutu <davidogutu@gmail.com>
2014-04-24 22:33:22 -0400

    csv support for all

--- data/admin.py
+++ data/admin.py
@@ -1,12 +1,40 @@
 from django.contrib import admin
 from data.models import  Node, DataPoint, AQI, Dylos, Alphasense, Met
+import csv
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+
+def export_as_csv(modeladmin, request, queryset):
+    """
+    Generic csv export admin action.
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = modeladmin.model._meta
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
+    writer = csv.writer(response)
+    field_names = [field.name for field in opts.fields]
+    # Write a first row with header information
+    writer.writerow(field_names)
+    # Write data rows
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+export_as_csv.short_description = "Export selected objects as csv file"
+
+
 
 # Register your models here.
 
-admin.site.register(Node)
-admin.site.register(DataPoint)
-admin.site.register(AQI)
 
-admin.site.register(Dylos)
-admin.site.register(Alphasense)
-admin.site.register(Met)
+class BaseAdmin(admin.ModelAdmin):
+    actions = [export_as_csv]
+
+admin.site.register(Node, BaseAdmin)
+admin.site.register(DataPoint, BaseAdmin)
+admin.site.register(AQI, BaseAdmin)
+
+admin.site.register(Dylos, BaseAdmin)
+admin.site.register(Alphasense, BaseAdmin)
+admin.site.register(Met, BaseAdmin)
